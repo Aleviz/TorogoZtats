1212868cd0daf000df43fc3bb53daa553493c6fd
RV20051 <rv20051@ues.edu.sv>
2023-05-21 14:51:43 -0600

    Configuracion para la conexion a la bbdd de MySQL y migracion de modelos a la bbdd de MySQL

--- TorogoZtats/settings.py
+++ TorogoZtats/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'TorogoZtatsWebApp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'TorogoZtats.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'torogoztats_hdp115',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- TorogoZtatsWebApp/models.py
+++ TorogoZtatsWebApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre_categoria=models.CharField(max_length=15)
+    nota=models.CharField(max_length=20)
+
+class Producto(models.Model):
+    nombre_producto=models.CharField(max_length=20)
+    unidad_medida=models.CharField(max_length=10)
+    cantidad=models.IntegerField()
+    categoria=models.ForeignKey(Categoria,null=True,blank=True,on_delete=models.CASCADE)
+
+class CanastaBasica(models.Model):
+    producto=models.ForeignKey(Producto,null=True,blank=True,on_delete=models.CASCADE)
+    precio=models.FloatField()
+    agno=models.IntegerField()
